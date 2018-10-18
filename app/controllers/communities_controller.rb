aa3253a463b78d1881a68c540c9f8445b7d78582
Dave Whitmer <dave@provsystems.com>
2018-10-18 17:21:28 -0400

    Stop erasing elements on community save

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -113,11 +113,11 @@ class CommunitiesController < ApplicationController
       @community.clinicians << Contact.find_by_id(params[:community][:clinician_id])
     end
 
-    @community.nursing_director =  nil if params[:community][:nursing_director].blank?
-    @community.executive_director = nil if params[:community][:executive_director].blank?
-    @community.admissions_director = nil if params[:community][:admissions_director].blank?
-    @community.marketing_director = nil if params[:community][:marketing_director].blank?
-    @community.zip_code = nil if params[:community][:zip_code].blank?
+    @community.nursing_director =  nil if params[:community].has_key?(:nursing_director) && params[:community][:nursing_director].blank?
+    @community.executive_director = nil if params[:community].has_key?(:executive_director) && params[:community][:executive_director].blank?
+    @community.admissions_director = nil if params[:community].has_key?(:admissions_director) && params[:community][:admissions_director].blank?
+    @community.marketing_director = nil if params[:community].has_key?(:marketing_director) && params[:community][:marketing_director].blank?
+    @community.zip_code = nil if params[:community].has_key?(:zip_code) && params[:community][:zip_code].blank?
 
     nursing_director =  params[:community][:nursing_director]
     executive_director = params[:community][:executive_director]
